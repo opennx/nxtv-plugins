20e22004b8d548ef0405432cfd7e8773010a2a6b
Martin W <martas@imm.cz>
2015-06-20 22:42:58 +0200

    pre-commit script added

---
+++ commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+chmod 755 cg
+chmod 755 dramatica_templates
+chmod 755 nxcg
+chmod 755 playout
+chmod 755 worker
+chmod 755 commit.sh
+
+chmod 644 cg/*.py
+chmod 644 dramatica_templates/*.py
+chmod 644 nxcg/*.py
+chmod 644 playout/*.py
+chmod 644 worker/*.py
